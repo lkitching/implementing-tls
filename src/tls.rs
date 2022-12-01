8c1d2b67f115874962bc2c9b70fe0b9dc4bda270
lkitching <lee.kitching@gmail.com>
2022-12-01 23:15:46 +0000

    Update BinarySerialisable to always write to a Vec

--- src/tls.rs
+++ src/tls.rs
@@ -7,11 +7,11 @@ trait FixedBinarySize {
 }
 
 trait BinarySerialisable {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize>;
-    fn write_to_vec(&self) -> io::Result<Vec<u8>> {
+    fn write_to(&self, writer: &mut Vec<u8>);
+    fn write_to_vec(&self) -> Vec<u8> {
         let mut v = Vec::new();
-        let bytes_written = self.write_to(&mut v);
-        Ok(v)
+        self.write_to(&mut v);
+        v
     }
 }
 
@@ -20,8 +20,8 @@ impl FixedBinarySize for u8 {
 }
 
 impl BinarySerialisable for u8 {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
-        writer.write([*self].as_slice())
+    fn write_to(&self, buf: &mut Vec<u8>) {
+        buf.push(*self);
     }
 }
 
@@ -104,9 +104,9 @@ impl FixedBinarySize for CipherSuiteIdentifier {
 }
 
 impl BinarySerialisable for CipherSuiteIdentifier {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+    fn write_to(&self, buf: &mut Vec<u8>) {
         let s = *self as u16;
-        writer.write(s.to_be_bytes().as_slice())
+        buf.extend_from_slice(s.to_be_bytes().as_slice())
     }
 }
 
@@ -117,9 +117,9 @@ struct ProtocolVersion {
 }
 
 impl BinarySerialisable for ProtocolVersion {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
-        let bytes = [self.major, self.minor];
-        writer.write(&bytes[..])
+    fn write_to(&self, buf: &mut Vec<u8>) {
+        buf.push(self.major);
+        buf.push(self.minor);
     }
 }
 
@@ -131,12 +131,9 @@ struct Random {
 }
 
 impl BinarySerialisable for Random {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
-        let mut buf = [0u8; 32];
-        let mut time_bytes = self.gmt_unix_time.to_be_bytes();
-        &mut buf[0..4].copy_from_slice(time_bytes.as_slice());
-        &mut buf[4..].copy_from_slice(self.random_bytes.as_slice());
-        writer.write(buf.as_slice())
+    fn write_to(&self, buf: &mut Vec<u8>) {
+        buf.extend_from_slice(self.gmt_unix_time.to_be_bytes().as_slice());
+        buf.extend_from_slice(self.random_bytes.as_slice());
     }
 }
 
@@ -158,54 +155,41 @@ struct ClientHello {
 }
 
 impl BinarySerialisable for ClientHello {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
-        let v = self.client_version.write_to(writer)?;
-        let r = self.random.write_to(writer)?;
-        let s = self.session_id.as_slice().write_to(writer)?;
-        let cs = self.cipher_suites.write_to(writer)?;
-        let cms = self.compression_methods.as_slice().write_to(writer)?;
-
-        Ok(v + r + s + cs + cms)
+    fn write_to(&self, writer: &mut Vec<u8>) {
+        self.client_version.write_to(writer);
+        self.random.write_to(writer);
+        self.session_id.as_slice().write_to(writer);
+        self.cipher_suites.write_to(writer);
+        self.compression_methods.as_slice().write_to(writer);
     }
 }
 
-fn write_elements<T: BinarySerialisable>(buf: &mut Vec<u8>, elems: &[T]) -> io::Result<usize> {
-    let mut bytes_written = 0;
-
+fn write_elements<T: BinarySerialisable>(buf: &mut Vec<u8>, elems: &[T]) {
     for e in elems.iter() {
-        bytes_written += e.write_to(buf)?;
+        e.write_to(buf);
     }
-
-    Ok(bytes_written)
 }
 
 impl <T: FixedBinarySize + BinarySerialisable> BinarySerialisable for &[T] {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+    fn write_to(&self, buf: &mut Vec<u8>) {
         let element_len = self.len() * T::binary_size();
 
-        let mut buf = Vec::with_capacity(1 + element_len);
         buf.push(element_len as u8);
-        let _ = write_elements(&mut buf, self);
-
-        writer.write(buf.as_slice())
+        write_elements(buf, self);
     }
 }
 
 impl BinarySerialisable for CipherSuites {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+    fn write_to(&self, buf: &mut Vec<u8>) {
         let elems = self.0.as_slice();
         let element_len = elems.len() * CipherSuiteIdentifier::binary_size();
 
-        // WARNING: cipher suites length is 2 bytes!
-        let mut buf = Vec::with_capacity(2 + element_len);
-
         // write length
+        // WARNING: cipher suites length is 2 bytes!
         buf.extend_from_slice((element_len as u16).to_be_bytes().as_slice());
 
         // write elements
-        write_elements(&mut buf, elems);
-
-        writer.write(buf.as_slice())
+        write_elements(buf, elems);
     }
 }
 
@@ -214,8 +198,8 @@ impl FixedBinarySize for CompressionMethods {
 }
 
 impl BinarySerialisable for CompressionMethods {
-    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
-        writer.write([*self as u8].as_slice())
+    fn write_to(&self, buf: &mut Vec<u8>) {
+        buf.push(*self as u8);
     }
 }
 
