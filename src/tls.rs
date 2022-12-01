6da3d67c011ecc1b391419647381e8c3a2917dd5
lkitching <lee.kitching@gmail.com>
2022-12-01 23:01:43 +0000

    Add serialisation trait and serialise ClientHello message

--- src/tls.rs
+++ src/tls.rs
@@ -1,6 +1,29 @@
-use std::io::{Write};
+use std::io::{self, Write};
 use chrono::{DateTime, Utc};
 use num_enum::{TryFromPrimitive};
+
+trait FixedBinarySize {
+    fn binary_size() -> usize;
+}
+
+trait BinarySerialisable {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize>;
+    fn write_to_vec(&self) -> io::Result<Vec<u8>> {
+        let mut v = Vec::new();
+        let bytes_written = self.write_to(&mut v);
+        Ok(v)
+    }
+}
+
+impl FixedBinarySize for u8 {
+    fn binary_size() -> usize { 1 }
+}
+
+impl BinarySerialisable for u8 {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        writer.write([*self].as_slice())
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, TryFromPrimitive)]
 #[repr(u8)]
@@ -76,10 +99,28 @@ enum CipherSuiteIdentifier {
     TLS_DH_anon_WITH_AES_256_CBC_SHA  = 0x003A,
 }
 
+impl FixedBinarySize for CipherSuiteIdentifier {
+    fn binary_size() -> usize { 2 }
+}
+
+impl BinarySerialisable for CipherSuiteIdentifier {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        let s = *self as u16;
+        writer.write(s.to_be_bytes().as_slice())
+    }
+}
+
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 struct ProtocolVersion {
     major: u8,
     minor: u8
+}
+
+impl BinarySerialisable for ProtocolVersion {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        let bytes = [self.major, self.minor];
+        writer.write(&bytes[..])
+    }
 }
 
 const TLS_VERSION: ProtocolVersion = ProtocolVersion { major: 3, minor: 1 };
@@ -89,6 +130,16 @@ struct Random {
     random_bytes: [u8; 28]
 }
 
+impl BinarySerialisable for Random {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        let mut buf = [0u8; 32];
+        let mut time_bytes = self.gmt_unix_time.to_be_bytes();
+        &mut buf[0..4].copy_from_slice(time_bytes.as_slice());
+        &mut buf[4..].copy_from_slice(self.random_bytes.as_slice());
+        writer.write(buf.as_slice())
+    }
+}
+
 impl Random {
     fn from_now(bytes: &[u8; 28]) -> Random {
         let ts = Utc::now().timestamp() as u32;
@@ -96,12 +147,76 @@ impl Random {
     }
 }
 
+struct CipherSuites(Vec<CipherSuiteIdentifier>);
+
 struct ClientHello {
     client_version: ProtocolVersion,
     random: Random,
     session_id: Vec<u8>,
-    cipher_suites: Vec<CipherSuiteIdentifier>,
+    cipher_suites: CipherSuites,
     compression_methods: Vec<CompressionMethods>,
+}
+
+impl BinarySerialisable for ClientHello {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        let v = self.client_version.write_to(writer)?;
+        let r = self.random.write_to(writer)?;
+        let s = self.session_id.as_slice().write_to(writer)?;
+        let cs = self.cipher_suites.write_to(writer)?;
+        let cms = self.compression_methods.as_slice().write_to(writer)?;
+
+        Ok(v + r + s + cs + cms)
+    }
+}
+
+fn write_elements<T: BinarySerialisable>(buf: &mut Vec<u8>, elems: &[T]) -> io::Result<usize> {
+    let mut bytes_written = 0;
+
+    for e in elems.iter() {
+        bytes_written += e.write_to(buf)?;
+    }
+
+    Ok(bytes_written)
+}
+
+impl <T: FixedBinarySize + BinarySerialisable> BinarySerialisable for &[T] {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        let element_len = self.len() * T::binary_size();
+
+        let mut buf = Vec::with_capacity(1 + element_len);
+        buf.push(element_len as u8);
+        let _ = write_elements(&mut buf, self);
+
+        writer.write(buf.as_slice())
+    }
+}
+
+impl BinarySerialisable for CipherSuites {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        let elems = self.0.as_slice();
+        let element_len = elems.len() * CipherSuiteIdentifier::binary_size();
+
+        // WARNING: cipher suites length is 2 bytes!
+        let mut buf = Vec::with_capacity(2 + element_len);
+
+        // write length
+        buf.extend_from_slice((element_len as u16).to_be_bytes().as_slice());
+
+        // write elements
+        write_elements(&mut buf, elems);
+
+        writer.write(buf.as_slice())
+    }
+}
+
+impl FixedBinarySize for CompressionMethods {
+    fn binary_size() -> usize { 1 }
+}
+
+impl BinarySerialisable for CompressionMethods {
+    fn write_to<W: Write>(&self, writer: &mut W) -> io::Result<usize> {
+        writer.write([*self as u8].as_slice())
+    }
 }
 
 struct TLSParameters {
@@ -114,7 +229,7 @@ fn send_client_hello<W: Write>(dest: &mut W, params: &TLSParameters) -> Result<(
         random: Random::from_now(&params.client_random.random_bytes),
         session_id: Vec::new(),
         compression_methods: vec![CompressionMethods::None],
-        cipher_suites: vec![CipherSuiteIdentifier::TLS_RSA_WITH_3DES_EDE_CBC_SHA]
+        cipher_suites: CipherSuites(vec![CipherSuiteIdentifier::TLS_RSA_WITH_3DES_EDE_CBC_SHA])
     };
 
     Ok(())
